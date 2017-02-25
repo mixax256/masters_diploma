eb5197394b94e05f4ebbd8b0ad8cda333e966b0e
mixax256 <mixax256@yandex.ru>
2017-02-25 15:18:17 +0300

    test commit

--- app/src/main/java/com/example/mixax256/masters_diploma/MainActivity.java
+++ app/src/main/java/com/example/mixax256/masters_diploma/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar); // тест изменения
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
